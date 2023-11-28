cc8b9d54fb7e3d1d5e8eee98940140454479bc9c
robinm2812 <robinm2812@gmail.com>
2023-11-28 16:18:00 -0500

    JavaScript initial changes

---
+++ javascript/recentandsaved.js
@@ -0,0 +1,34 @@
+var $ = function(id) { return document.getElementById(id); };
+
+var loadRecipes = function (load_type) {
+    var url = window.location.href.substring(0, window.location.href.lastIndexOf("/") + 1);
+    let load_type_element = $(load_type);
+    if (localStorage.getItem(load_type)) {
+        $("empty_list_" + load_type).style.display = "none";
+        for (let recipe of localStorage.getItem(load_type).split("|")) {
+            let recipe_title = document.createElement('h2');
+            let recipe_link = document.createElement('a');
+            recipe_link.href = recipe;
+            let recipe_name = document.createTextNode(recipes[recipe.split("/")[1].split(".")[0]]);
+            recipe_link.appendChild(recipe_name);
+            recipe_title.appendChild(recipe_link);
+            load_type_element.appendChild(recipe_title);
+        }
+    }
+};
+
+var loadRecentRecipes = function () {
+    var load_type = "recentrecipes";
+    loadRecipes(load_type);
+}
+
+var loadSavedRecipes = function () {
+    var load_type = "savedrecipes";
+    loadRecipes(load_type);
+}
+
+window.onload = function () {
+    loadRecentRecipes();
+    loadSavedRecipes();
+    // $("share").href += window.location.href;
+}
---
+++ javascript/recipe.js
@@ -0,0 +1,30 @@
+var $ = function(id) { return document.getElementById(id);};
+
+var storeRecipe = function(recipe_type) {
+    var href_parts = window.location.href.split("/").reverse()
+    var recipe_location = href_parts[1] + "/" + href_parts[0];
+    if(localStorage.getItem(recipe_type)){
+        localStorage.setItem(recipe_type, recipe_location + "|" + localStorage.getItem(recipe_type));
+    } else {
+        localStorage.setItem(recipe_type, recipe_location);
+    }
+    var recipe_set = new Set(localStorage.getItem(recipe_type).split("|"));
+    localStorage.setItem(recipe_type, [...recipe_set].join("|"));
+};
+
+var saveRecipe = function() {
+    var recipe_type = "savedrecipes";
+    storeRecipe(recipe_type);
+};
+
+var recentRecipes = function() {
+    var recipe_type = "recentrecipes";
+    storeRecipe(recipe_type);
+    localStorage.setItem(recipe_type, localStorage.getItem(recipe_type).split("|").slice(0, 5).join("|"));
+};
+
+window.onload = function() {
+    $('print_button').onclick = () => {window.print()};
+    $('save-recipe-button').onclick = saveRecipe;
+    recentRecipes();
+};
