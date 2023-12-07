2249273a0699120c26ef7b8d732d0e85e5aded8a
robinm2812 <robinm2812@gmail.com>
2023-12-06 19:33:26 -0500

    jQuery initial changes

--- javascript/home.js
+++ javascript/home.js
@@ -1,59 +1,59 @@
-let timeoutid = "";
-let intervalid = "";
-let count = 0;
+$(document).ready(function () {
+    let timeoutid = "";
+    let intervalid = "";
 
-var nextSlide = function () {
-    let slides = document.getElementsByClassName("mySlides");
-    let temp = slides[0].cloneNode(true);
-    for (i = 0; i < slides.length; i++) {
-        if (i + 1 == slides.length) {
-            slides[i].parentNode.replaceChild(temp, slides[i]);
+    function nextSlide() {
+        let slides = $(".mySlides");
+        let temp = slides[0].cloneNode(true);
+        for (i = 0; i < slides.length; i++) {
+            if (i + 1 == slides.length) {
+                slides[i].parentNode.replaceChild(temp, slides[i]);
+            }
+            else {
+                slides[i].parentNode.replaceChild(slides[i + 1].cloneNode(true), slides[i]);
+            }
         }
-        else {
-            slides[i].parentNode.replaceChild(slides[i + 1].cloneNode(true), slides[i]);
+    };
+
+    function prevSlide() {
+        let slides = $(".mySlides");
+        let temp = slides[slides.length - 1].cloneNode(true);
+        for (i = slides.length - 1; i > -1; i--) {
+            if (i - 1 == -1) {
+                slides[i].parentNode.replaceChild(temp, slides[i]);
+            }
+            else {
+                slides[i].parentNode.replaceChild(slides[i - 1].cloneNode(true), slides[i]);
+            }
         }
-    }
-};
+    };
 
-var prevSlide = function () {
-    let slides = document.getElementsByClassName("mySlides");
-    let temp = slides[slides.length - 1].cloneNode(true);
-    for (i = slides.length - 1; i > -1; i--) {
-        if (i - 1 == -1) {
-            slides[i].parentNode.replaceChild(temp, slides[i]);
+    function autoSlides() {
+        intervalid = setInterval(nextSlide, 2800);
+    };
+
+    $("#prev_carousel").on("click", function () {
+        if (timeoutid !== "") {
+            clearTimeout(timeoutid);
         }
-        else {
-            slides[i].parentNode.replaceChild(slides[i - 1].cloneNode(true), slides[i]);
+        if (intervalid !== "") {
+            clearInterval(intervalid);
         }
-    }
-};
+        prevSlide();
+        timeoutid = setTimeout(autoSlides, 2000);
+    });
 
-var autoSlides = function () {
-    intervalid = setInterval(nextSlide, 2800);
-};
+    $("#next_carousel").on("click", function () {
+        if (timeoutid != "") {
+            clearTimeout(timeoutid);
+        }
+        if (intervalid != "") {
+            clearInterval(intervalid);
+        }
+        nextSlide();
+        timeoutid = setTimeout(autoSlides, 2000);
+    });
 
-var button_prevSlide = function () {
-    if (timeoutid !== "") {
-        clearTimeout(timeoutid);
-    }
-    if (intervalid !== "") {
-        clearInterval(intervalid);
-    }
-    prevSlide();
-    timeoutid = setTimeout(autoSlides, 2000);
-};
-
-var button_nextSlide = function () {
-    if (timeoutid != "") {
-        clearTimeout(timeoutid);
-    }
-    if (intervalid != "") {
-        clearInterval(intervalid);
-    }
-    nextSlide();
-    timeoutid = setTimeout(autoSlides, 2000);
-};
-
-window.onload = function () {
     autoSlides();
-};
+
+});
